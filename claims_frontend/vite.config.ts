3d4785b711a7bd353da8397c0b12550ef463b68e
Nickitarius <pilnov.nickita@yandex.ru>
2024-10-01 03:11:36 +0800

    Treeshaking and pruning

--- claims_frontend/vite.config.ts
+++ claims_frontend/vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { readFileSync } from 'node:fs';
 import { resolve } from 'node:path';
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vuetify()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
